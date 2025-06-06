f738efab727af236525d12e1de8994ff98166cb2
Rafael Dolinski <rafaeldolinski14@gmail.com>
2025-06-06 20:43:44 -0300

    fix: ajustar arquivo JSON para correção de dados/configuração

--- aplicao-em-ReactJS/src/main.jsx
+++ aplicao-em-ReactJS/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ptBR } from '@mui/material/locale';
 import AppRoutes from './routes/AppRoutes';
+
+// Criar o tema do Material UI com idioma português
+const theme = createTheme({}, ptBR);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRoutes />
+    <ThemeProvider theme={theme}>
+      <AppRoutes />
+    </ThemeProvider>
   </React.StrictMode>
 );
